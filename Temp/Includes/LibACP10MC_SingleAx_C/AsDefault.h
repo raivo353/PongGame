12b6c9cf07c3e1a0a43a741e220565ceae90f85b
raivo <91531605+raivo353@users.noreply.github.com>
2026-03-09 11:34:47 +0100

    Update .gitignore

---
+++ Temp/Includes/LibACP10MC_SingleAx_C/AsDefault.h
@@ -0,0 +1,6 @@
+#ifndef _LIBACP10MC_SINGLEAX_C_DEFAULT_697213632
+#define _LIBACP10MC_SINGLEAX_C_DEFAULT_697213632
+#include "../AsDefault.h"
+#include <bur/plctypes.h>
+#include <bur/plc.h>
+#endif
---
+++ Temp/Includes/LibACP10MC_SingleAx_C/Basic/AsDefault.h
@@ -0,0 +1,8 @@
+#ifndef _LIBACP10MC_SINGLEAX_C_BASIC_DEFAULT_1357688817
+#define _LIBACP10MC_SINGLEAX_C_BASIC_DEFAULT_1357688817
+#include "../AsDefault.h"
+#include <bur/plctypes.h>
+#include <bur/plc.h>
+#include <basicTYP.h>
+#include <basicVAR.h>
+#endif
