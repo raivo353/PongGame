aaf9e76840863543a04367b87aad08b050132feb
raivo <91531605+raivo353@users.noreply.github.com>
2026-03-09 11:09:36 +0100

    Removed ignored files

--- Temp/Includes/Timer_CPP/TonTimer/AsDefault.h
+++
@@ -1,7 +0,0 @@
-#ifndef _TIMER_CPP_TONTIMER_DEFAULT_1096765726
-#define _TIMER_CPP_TONTIMER_DEFAULT_1096765726
-#include "../AsDefault.h"
-#include <bur/plctypes.h>
-#include <bur/plc.h>
-#include <tontimerVAR.h>
-#endif
